906a01b217a42db101a5e217aff110a1322d90ca
tsimeonov <tsimeonov12@gmail.com>
2023-03-26 19:34:18 +0200

    Hiding the previous tab

--- code/app.js
+++ code/app.js
@@ -58,6 +58,10 @@ function changeTabPanel(e) {
   const tabContainer = targetTab.parentNode;
   const mainContainer = tabContainer.parentNode;
 
+  mainContainer
+    .querySelectorAll("[role='tabpanel']")
+    .forEach((panel) => panel.setAttribute("hidden", true));
+
   mainContainer.querySelector([`#${targetPanel}`]).removeAttribute("hidden");
 
   // console.log(mainContainer);
