f12cb8389766da433794bdffdefebd0dee660917
tsimeonov <tsimeonov12@gmail.com>
2023-03-26 17:28:14 +0200

    Setting up the keyboard navigation

--- code/app.js
+++ code/app.js
@@ -1,3 +1,4 @@
+// Navigation
 const nav = document.querySelector(".primary-navigation");
 const navToggle = document.querySelector(".mobile-nav-toggle");
 
@@ -11,3 +12,40 @@ navToggle.addEventListener("click", () => {
     navToggle.setAttribute("aria-expanded", false);
   }
 });
+
+// Tabs
+
+const tabList = document.querySelector('[role="tablist"]');
+const tabs = tabList.querySelectorAll('[role="tab"]');
+
+tabList.addEventListener("keydown", changeTabFocus);
+
+let tabFocus = 0;
+function changeTabFocus(e) {
+  const keyDownLeft = 37;
+  const keyDownRight = 39;
+
+  // Change the tabindex of the current tab to -1
+  if (e.keyCode === keyDownLeft || e.keyCode === keyDownRight) {
+    tabs[tabFocus].setAttribute("tabindex", -1);
+  }
+
+  // If the right key is pushed, move to the next tab on the right
+  if (e.keyCode === keyDownRight) {
+    tabFocus++;
+    if (tabFocus >= tabs.length) {
+      tabFocus = 0;
+    }
+  }
+
+  // If the left key is pushed, move to the next tab on the left
+  if (e.keyCode === keyDownLeft) {
+    tabFocus--;
+    if (tabFocus < 0) {
+      tabFocus = tabs.length - 1;
+    }
+  }
+
+  tabs[tabFocus].setAttribute("tabindex", 0);
+  tabs[tabFocus].focus();
+}
