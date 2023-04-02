76d3731000fa0a8863aed5915254e38f0fc10669
tsimeonov <tsimeonov12@gmail.com>
2023-04-02 09:22:30 +0200

    refactoring the app.js

--- code/app.js
+++ code/app.js
@@ -31,24 +31,22 @@ function changeTabFocus(e) {
 
   if (e.keyCode === keydownLeft || e.keyCode === keydownRight) {
     tabs[tabFocus].setAttribute("tabindex", -1);
-  }
 
-  if (e.keyCode === keydownRight) {
-    tabFocus++;
-    if (tabFocus >= tabs.length) {
-      tabFocus = 0;
+    if (e.keyCode === keydownRight) {
+      tabFocus++;
+      if (tabFocus >= tabs.length) {
+        tabFocus = 0;
+      }
+    } else if (e.keyCode === keydownLeft) {
+      tabFocus--;
+      if (tabFocus < 0) {
+        tabFocus = tabs.length - 1;
+      }
     }
-  }
 
-  if (e.keyCode === keydownLeft) {
-    tabFocus--;
-    if (tabFocus < 0) {
-      tabFocus = tabs.length - 1;
-    }
+    tabs[tabFocus].setAttribute("tabindex", 0);
+    tabs[tabFocus].focus();
   }
-
-  tabs[tabFocus].setAttribute("tabindex", 0);
-  tabs[tabFocus].focus();
 }
 
 function changeTabPanel(e) {
@@ -65,17 +63,21 @@ function changeTabPanel(e) {
 
   targetTab.setAttribute("aria-selected", true);
 
-  mainContainer
-    .querySelectorAll("[role='tabpanel']")
-    .forEach((panel) => panel.setAttribute("hidden", true));
+  hideContent(mainContainer, "[role='tabpanel']");
 
-  mainContainer.querySelector([`#${targetPanel}`]).removeAttribute("hidden");
+  showContent(mainContainer, [`#${targetPanel}`]);
 
-  mainContainer
-    .querySelectorAll("picture")
-    .forEach((picture) => picture.setAttribute("hidden", true));
+  hideContent(mainContainer, "picture");
 
-  mainContainer.querySelector([`#${targetImage}`]).removeAttribute("hidden");
+  showContent(mainContainer, [`#${targetImage}`]);
+}
 
-  // console.log(mainContainer);
+function hideContent(parent, content) {
+  parent
+    .querySelectorAll(content)
+    .forEach((item) => item.setAttribute("hidden", true));
+}
+
+function showContent(parent, content) {
+  parent.querySelector(content).removeAttribute("hidden");
 }
